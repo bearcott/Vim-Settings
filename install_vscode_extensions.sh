331418c179087a727ccfda52849dcb5f4bea97bb
Benny Yan <bnynn.gml@gmail.com>
2018-07-27 14:30:11 -0700

    vscode extensions

---
+++ install_vscode_extensions.sh
@@ -0,0 +1,27 @@
+code --install-extension PeterJausovec.vscode-docker
+code --install-extension RoscoP.ActiveFileInStatusBar
+code --install-extension Tyriar.lorem-ipsum
+code --install-extension auiworks.amvim
+code --install-extension daltonjorge.scala
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension dragos.scala-lsp
+code --install-extension eamodio.gitlens
+code --install-extension eg2.tslint
+code --install-extension esbenp.prettier-vscode
+code --install-extension flowtype.flow-for-vscode
+code --install-extension gabro.vscode-scalafmt
+code --install-extension HookyQR.beautify
+code --install-extension James-Yu.latex-workshop
+code --install-extension jpoissonnier.vscode-styled-components
+code --install-extension JuanBlanco.solidity
+code --install-extension kumar-harsh.graphql-for-vscode
+code --install-extension mgmcdermott.vscode-language-babel
+code --install-extension redhat.java
+code --install-extension stevencl.addDocComments
+code --install-extension vscjava.vscode-java-debug
+code --install-extension vscjava.vscode-java-pack
+code --install-extension vscjava.vscode-java-test
+code --install-extension vscjava.vscode-maven
+code --install-extension vscodevim.vim
+code --install-extension zhuangtongfa.Material-theme
+
