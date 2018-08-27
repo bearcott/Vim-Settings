58c396a3f165c9b73e505c6ca5c958bcbde882f0
Benny Yan <bnynn.gml@gmail.com>
2018-08-27 10:22:38 -0700

    install script

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 cp .* ../;
+cp -R .vim ../;
 source .bash_profile;
+source <(curl -s https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash);
+source <(curl -s https://raw.githubusercontent.com/garabik/grc/master/grc.bashrc);
+source install_vscode_extensions.sh
 
