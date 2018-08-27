6dd3d7b342554b3d2a6962e1259fe495e11c1838
Benny Yan <bnynn.gml@gmail.com>
2018-08-27 10:35:40 -0700

    oops install

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 cp .* ../;
 cp -R .vim ../;
-curl -s https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash > git-completion.sh;
-curl -s https://raw.githubusercontent.com/garabik/grc/master/grc.bashrc > grc.bashrc;
+curl -s https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash > ../git-completion.sh;
+curl -s https://raw.githubusercontent.com/garabik/grc/master/grc.bashrc > ../grc.bashrc;
 source ../.bash_profile;
 source install_vscode_extensions.sh
 
