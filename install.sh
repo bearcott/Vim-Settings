f7a5e09749ad8b6ab3b47409c3efd00627b3b57d
Benny Yan <bnynn.gml@gmail.com>
2018-09-05 11:17:42 -0500

    change install to get the gitcompletion and w/e
    added vundle

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 cp .* ~/;
 cp -R .vim ~/;
-#curl -s https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash > ../git-completion.sh;
-#curl -s https://raw.githubusercontent.com/garabik/grc/master/grc.bashrc > ../grc.bashrc;
+curl -s https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash > ../git-completion.sh;
+curl -s https://raw.githubusercontent.com/garabik/grc/master/grc.bashrc > ../grc.bashrc;
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 source ~/.bash_profile;
 source install_vscode_extensions.sh
 
